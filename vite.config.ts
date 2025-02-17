68ded65de61c3458c33830f154423e6280b3b918
Kathy <kathy.kao@xuenn.com>
2025-02-17 17:49:39 +0800

    add ts initial setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import eslint from 'vite-plugin-eslint';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   root: 'src',
@@ -14,5 +15,12 @@ export default defineConfig({
       },
     },
   },
-  plugins: [eslint()],
+  plugins: [
+    eslint(),
+    checker({
+      typescript: true,
+      // eslint: { lintCommand: 'eslint "**/*.{js,ts}"' },
+      eslint: { lintCommand: 'eslint "{ts,js,api,types}**/*.{js,ts}"' },
+    }),
+  ],
 });
